8713134a5464f74a28137d4d7c62491e900d51d1
ferperezv <frpvillatoro@gmail.com>
2019-11-19 15:44:51 +0200

    Adding comments and removing not necessary lines

--- R/shrink.seg.ai.R
+++ R/shrink.seg.ai.R
@@ -6,14 +6,14 @@ shrink.seg.ai<-function(chr.seg){
   if(nrow(chr.seg) > 1){
     new.chr <- chr.seg
     seg.class <- c(1)
+    #Check if the cnv values for A and B allele in the previous segment is equal in the current segment
     for(j in 2:nrow(new.chr)){
       seg_test <- new.chr[(j-1),7] == new.chr[j,7] & new.chr[(j-1),8] == new.chr[j,8]
       if(seg_test){
-        seg.class <- c(seg.class, seg.class[j-1])
-      }
-      if(!seg_test){
-        seg.class <- c(seg.class, seg.class[j-1]+1)
-      }
+          seg.class <- c(seg.class, seg.class[j-1])
+        }else{
+    seg.class <- c(seg.class, seg.class[j-1]+1)
+  }
     }
     for(j in unique(seg.class)){
       new.chr[seg.class %in% j,4] <- max(new.chr[seg.class %in% j,4])
